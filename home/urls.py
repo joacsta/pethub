6271128fc2135c1e7671f20526dd251e429f9b80
João Costa <jvcosta1502@gmail.com>
2026-08-30 19:06:06 -0300

    cria endpoint 'conceito' e 'historias', edita urls

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = "home"
 
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("conceito/", views.conceito, name="conceito"),
+    path("depoimentos/", views.historias, name="depoimentos"),
+]
--- home/views.py
+++ home/views.py
@@ -4,5 +4,15 @@ from django.http import HttpRequest
 
 # Create your views here.
 def home(request: HttpRequest):
-    context = {}
+    context = {"title": "PetHub"}
     return render(request, "home/index.html", context)
+
+
+def conceito(request: HttpRequest):
+    context = {"title": "PetHub - Conceito"}
+    return render(request, "home/conceito.html", context)
+
+
+def historias(request: HttpRequest):
+    context = {"title": "PetHub - Depoimentos"}
+    return render(request, "home/historias.html", context)
